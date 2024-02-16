56e113681f327483cf60687c867f8c035e7b9b8e
katdanev <138736957+katdanev@users.noreply.github.com>
2024-02-16 17:48:41 -0330

    basic CLI to init  the app, add default and help

--- myapp.js
+++ myapp.js
@@ -3,7 +3,32 @@ global.DEBUG = true;
 const fs = require("fs");
 
 
-const myArgs = process.argv.slice(2); // get 2 arguments from the command line
 
+const myArgs = process.argv.slice(2);
 
 if(DEBUG) if(myArgs.length >= 1) console.log('the myapp.args: ', myArgs);
+
+switch (myArgs[0]) {
+  case 'init':
+  case 'i':
+      if(DEBUG) console.log(myArgs[0], ' - initialize the app.');
+    
+      break;
+  case 'config':
+  case 'c':
+      if(DEBUG) console.log(myArgs[0], ' - display the configuration file');
+    
+      break;
+  case 'token':
+  case 't':
+      if(DEBUG) console.log(myArgs[0], ' - generate a user token');
+   
+      break;  
+  case '--help':
+  case '--h':
+  default:
+      fs.readFile(__dirname + "/usage.txt", (error, data) => {
+          if(error) throw error;
+          console.log(data.toString());
+      });
+}
